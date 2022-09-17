2a9066aba3a8df33260266d32b978d89dd6e593e
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-09-18 00:47:32 +0600

    header section done

---
+++ script.js
@@ -0,0 +1,22 @@
+
+$('.owl-carousel').owlCarousel({
+loop:true,
+margin:10,
+nav:false,
+autoplay:true,
+
+autoplayTimeout: 4000,
+animateOut: 'fadeOut',
+dots:false,
+responsive:{
+0:{
+    items:1
+},
+600:{
+    items:1
+},
+1000:{
+    items:1
+}
+}
+})
