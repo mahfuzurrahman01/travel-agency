09b07a9d2d5191febdbc15393202351c7deb3115
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-09-18 14:00:24 +0600

    main section added

--- script.js
+++ script.js
@@ -20,3 +20,16 @@ responsive:{
 }
 }
 })
+
+const field = document.getElementById('img');
+
+let num = 0;
+let image = ['images/image1.jpg','images/image2.jpg','images/image3.jpg']
+setInterval(()=>{
+    if(num === image.length){
+        num = 0;
+    }
+ let imgLink = image[num];
+ field.setAttribute('src',imgLink)
+ num++
+},2500)
